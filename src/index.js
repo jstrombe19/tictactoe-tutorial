cd7a5156b1d6b9ea64fe1e6296ad781bcd34a181
jstrombe19 <jaredstromberg@gmail.com>
2020-04-16 21:19:05 -0700

    Implemented bold step font on hover using CSS.

--- src/index.js
+++ src/index.js
@@ -15,47 +15,19 @@ function Square(props) {
   }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-
+  
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}  
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)}  
       />
-    );
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      );
     }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
+    
+    render() {
+      return (
+        <div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -77,15 +49,77 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    }
+  }
+  
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      console.log(displayMove(i))
+
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      })
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+  
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move 
+      ? 'Go to move #' + move 
+      : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+    
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -98,6 +132,40 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function displayMove(square) {
+  const columns = {
+    column1: [0, 3, 6], // square % 3 === 0
+    column2: [1, 4, 7], // square % 3 === 1
+    column3: [2, 5, 8]  // square % 3 === 2
+  }
+  const rows = {
+    row1: [0, 1, 2], 
+    row2: [3, 4, 5],
+    row3: [6, 7, 8]
+  }
+
+  let column;
+  let row;
+
+  switch(square % 3) {
+    case 0:
+      column = 1;
+      break;
+    case 1:
+      column = 2;
+      break;
+    case 2:
+      column = 3;
+      break;
+    default: 
+      console.log('This is tic-tac-toe, not checkers!');
+      break;
+  }
+
+  return column;
+
+}
 
 
 function calculateWinner(squares) {
